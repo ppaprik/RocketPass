923af5af0b8c2238c7f8b6ea3a753eb816162496
psychotronixx <sabo.patrik1@gmail.com>
2025-01-16 10:39:06 +0100

    updated data

--- Website/src/components/HelloWorld.vue
+++ Website/src/components/HelloWorld.vue
@@ -1,7 +1,10 @@
-<script setup lang="ts">
-defineProps<{
-  msg: string
-}>()
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
 </script>
 
 <template>
@@ -9,7 +12,7 @@ defineProps<{
     <h1 class="green">{{ msg }}</h1>
     <h3>
       You’ve successfully created a project with
-      <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
+      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
     </h3>
   </div>
